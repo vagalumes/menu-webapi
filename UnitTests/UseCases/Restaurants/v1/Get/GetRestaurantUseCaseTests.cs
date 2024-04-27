a198f90e993b4e3263ff74da730cd58665da8023
Felipe Rodrigues <lipe2008.lipao@gmail.com>
2024-04-27 16:59:58 -0300

    feat(Application): update entities and add UnitOfWork service
    This commit includes updates to the MenuItem and MenuItemsImages entities, addition of a DateOnlyConverter, and implementation of a UnitOfWork service. These changes enhance the functionality and efficiency of the application.

---
+++ UnitTests/UseCases/Restaurants/v1/Get/GetRestaurantUseCaseTests.cs
@@ -0,0 +1,55 @@
+﻿using Application.Shared.Entities;
+using Application.UseCases.Restaurants.v1.GetRestaurantUsecase;
+using Application.UseCases.Restaurants.v1.GetRestaurantUsecase.Abstractions;
+using Application.UseCases.Restaurants.v1.GetRestaurantUsecase.Models;
+using Application.UseCases.Restaurants.v1.GetRestaurantUsecase.Services.Repositories.Abstractions;
+using AutoFixture;
+using Moq;
+
+namespace UnitTests.UseCases.Restaurants.v1.Get;
+
+public class GetRestaurantUseCaseTests
+{
+    private readonly Mock<IRestaurantRepository> _mockRepository = new();
+    private readonly Mock<IOutputPort> _mockOutputPort = new();
+    private readonly Fixture _fixture = new();
+    private readonly GetRestaurantUseCase _service;
+    
+    public GetRestaurantUseCaseTests()
+    {
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        _service = new GetRestaurantUseCase(_mockRepository.Object);
+        _service.SetOutputPort(_mockOutputPort.Object);
+    }
+    
+    [Fact]
+    public async Task Should_Return_Restaurant_Not_Found()
+    {
+        // Arrange
+        var restaurantId = Guid.NewGuid();
+        _mockRepository.Setup(x => x.GetRestaurant(restaurantId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync((Restaurant?)null);
+        
+        // Act
+        await _service.ExecuteAsync(restaurantId, CancellationToken.None);
+        
+        // Assert
+        _mockOutputPort.Verify(x => x.RestaurantNotFound(), Times.Once);
+    }
+    
+    [Fact]
+    public async Task Should_Return_Restaurant_Found()
+    {
+        // Arrange
+        var restaurantId = Guid.NewGuid();
+        var restaurant = _fixture.Create<Restaurant>();
+        _mockRepository.Setup(x => x.GetRestaurant(restaurantId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(restaurant);
+        
+        // Act
+        await _service.ExecuteAsync(restaurantId, CancellationToken.None);
+        
+        // Assert
+        _mockOutputPort.Verify(x => x.RestaurantFound(It.IsAny<GetRestaurantModel>()), Times.Once);
+    }
+}
---
+++ UnitTests/UseCases/Restaurants/v1/Update/UpdateRestaurantDependencyInjectionTests.cs
@@ -0,0 +1,29 @@
+﻿using Application.UseCases.Restaurants.v1.UpdateRestaurantUseCase;
+using Application.UseCases.Restaurants.v1.UpdateRestaurantUseCase.Abstractions;
+using FluentAssertions;
+using Menu_WebApi.Modules;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitTests.UseCases.Restaurants.v1.Update;
+
+public class UpdateRestaurantDependencyInjectionTests
+{
+    [Fact]
+    public void UpdateRestaurantUseCase_ShouldBeRegistered()
+    {
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.Test.json").Build();
+        var services = new ServiceCollection();
+        
+        
+        services
+            .ConfigureDataBase(configuration)
+            .AddUpdateRestaurantUseCase();
+
+        var provider = services.BuildServiceProvider();
+
+        var useCase = provider.GetService<IUpdateRestaurantUseCase>();
+
+        useCase.Should().NotBeNull();
+    }
+}
