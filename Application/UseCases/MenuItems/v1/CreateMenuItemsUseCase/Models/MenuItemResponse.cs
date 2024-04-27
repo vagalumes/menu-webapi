1c9ef5c7914df35c92c71e44e1a6206b55e0de68
Vinny Moura <vmmoura482@gmail.com>
2024-04-27 18:35:05 -0300

    [ FIX ] Changes made to resolve issues reported by sonar

--- Application/UseCases/MenuItems/v1/CreateMenuItemsUseCase/Models/MenuItemResponse.cs
+++ Application/UseCases/MenuItems/v1/CreateMenuItemsUseCase/Models/MenuItemResponse.cs
@@ -4,12 +4,14 @@ namespace Application.UseCases.MenuItems.v1.CreateMenuItemsUseCase.Models;
 
 public class MenuItemResponse()
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public decimal Price { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+    public decimal Price { get; init; }
 
     public MenuItemResponse(MenuItem menuItem) : this()
     {
+        Id = menuItem.Id;
         Name = menuItem.Name;
         Description = menuItem.Description;
         Price = menuItem.Price;
